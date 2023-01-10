8c028f9fc4754b16a1b9eccace1354b8c4e2437f
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-10 15:46:11 +0530

    updated Calculator.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,7 @@ export default function Navbar() {
     <nav className="navbar">
       <h1 className="header-title"> Math Magicians</h1>
       <ul className="navlinks">
-        <li className="link"><Link to="/" className="">Home</Link></li>
         <li className="link"><Link to="/calculator" className="">Calculator</Link></li>
-        <li className="link last"><Link to="/quote" className="">Quote</Link></li>
       </ul>
     </nav>
   );
