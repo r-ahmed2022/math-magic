77febbba4843f143988e130f6c3b617ddb907d5a
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-10 15:55:36 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,46 +2,34 @@ import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Navbar from './components/Navbar';
-import calculate from './logic/calculate';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      total: null,
-      next: 0,
-      operation: null,
+
     };
   }
 
-    handleClick = (event) => {
-      const data = event.target.value;
-      const state = calculate(this.state, data);
-      this.setState(state);
-      document.getElementById('numinput').value = state.next;
-    };
-
-    render() {
-      return (
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route
-              path="/Calculator"
-              element={
-                // eslint-disable-next-line react/jsx-wrap-multilines, react/jsx-first-prop-new-line
-                <Calculator onclick={this.handleClick} state={this.state} />
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/Calculator"
+            element={
+              <Calculator onclick={this.handleClick} state={this.state} />
 }
-            />
-            <Route
-              path="*"
-              element={
-                // eslint-disable-next-line react/jsx-wrap-multilines, react/jsx-first-prop-new-line
-                <Calculator onclick={this.handleClick} state={this.state} />
+          />
+          <Route
+            path="*"
+            element={
+              <Calculator onclick={this.handleClick} state={this.state} />
 }
-            />
-          </Routes>
-        </div>
-      );
-    }
+          />
+        </Routes>
+      </div>
+    );
+  }
 }
