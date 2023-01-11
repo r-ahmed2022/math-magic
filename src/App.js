e9ef2ebdf10b02e0b1e2b6523dca749b57b98d04
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-11 15:58:56 +0530

    added event handler to calculator component

--- src/App.js
+++ src/App.js
@@ -2,34 +2,39 @@ import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Navbar from './components/Navbar';
+import calculate from './logic/calculate';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      total: '',
+      next: 0,
+      operation: null,
     };
   }
 
-  render() {
-    return (
-      <div className="container">
-        <Navbar />
-        <Routes>
-          <Route
-            path="/Calculator"
-            element={
-              <Calculator onclick={this.handleClick} state={this.state} />
+    handleClick = (event) => {
+      const data = event.target.value;
+      const state = calculate(this.state, data);
+      this.setState(state);
+      document.getElementById('numinput').value = state.next;
+    };
+
+    render() {
+      return (
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route
+              path="/Calculator"
+              element={
+                // eslint-disable-next-line react/jsx-wrap-multilines, react/jsx-first-prop-new-line
+                <Calculator onclick={this.handleClick} state={this.state} />
 }
-          />
-          <Route
-            path="*"
-            element={
-              <Calculator onclick={this.handleClick} state={this.state} />
-}
-          />
-        </Routes>
-      </div>
-    );
-  }
+            />
+          </Routes>
+        </div>
+      );
+    }
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,18 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+
+/* eslint-disable react/prop-types */
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import './Calculator.css';
 
 export default class Calculator extends Component {
   render() {
-    const {
-      onclick, total, next, onchange,
-    } = this.props;
-
+    const { onclick, state, onchange } = this.props;
+    // eslint-disable-next-line no-unused-vars
+    const { total, next, operation } = state;
     return (
       <main className="calculator-section">
         <div className="left">
@@ -19,37 +25,37 @@ export default class Calculator extends Component {
             <input type="text" name="numinput" id="numinput" className="numinput" maxLength="20" value={next} placeholder={total} onChange={onchange} />
           </div>
           <div className="btn-div">
-            <input type="button" className="btn" value="AC" />
-            <input type="button" className="btn" value="+/-" />
-            <input type="button" className="btn" value="%" />
-            <input type="button" className="btn symbol" value="&divide;" />
+            <input type="button" className="btn" value="AC" onClick={onclick} />
+            <input type="button" className="btn" value="+/-" onClick={onclick} />
+            <input type="button" className="btn" value="%" onClick={onclick} />
+            <input type="button" className="btn symbol" value="&divide;" onClick={onclick} />
           </div>
           <div className="btn-div">
-            <input type="button" className="btn" value="7" />
-            <input type="button" className="btn" value="8" />
-            <input type="button" className="btn" value="9" />
+            <input type="button" className="btn" value="7" onClick={onclick} />
+            <input type="button" className="btn" value="8" onClick={onclick} />
+            <input type="button" className="btn" value="9" onClick={onclick} />
             <input type="button" className="btn symbol" value="x" onClick={onclick} />
           </div>
           <div className="btn-div">
-            <input type="button" className="btn" value="4" />
-            <input type="button" className="btn" value="5" />
-            <input type="button" className="btn" value="6" />
-            <input type="button" className="btn symbol" value="-" />
+            <input type="button" className="btn" value="4" onClick={onclick} />
+            <input type="button" className="btn" value="5" onClick={onclick} />
+            <input type="button" className="btn" value="6" onClick={onclick} />
+            <input type="button" className="btn symbol" value="-" onClick={onclick} />
           </div>
 
           <div className="btn-div">
-            <input type="button" className="btn" value="1" />
-            <input type="button" className="btn" value="2" />
-            <input type="button" className="btn" value="3" />
-            <input type="button" className="btn symbol" value="+" />
+            <input type="button" className="btn" value="1" onClick={onclick} />
+            <input type="button" className="btn" value="2" onClick={onclick} />
+            <input type="button" className="btn" value="3" onClick={onclick} />
+            <input type="button" className="btn symbol" value="+" onClick={onclick} />
           </div>
           <div className="btn-div-last">
             <div className="div-0">
-              <input type="button" className="btn" value="0" />
+              <input type="button" className="btn" value="0" onClick={onclick} />
             </div>
             <div className="div-last">
-              <input type="button" className="btn btn-last" value="." />
-              <input type="button" className="btn symbol btn-equal" value="=" />
+              <input type="button" className="btn btn-last" value="." onClick={onclick} />
+              <input type="button" className="btn symbol btn-equal" value="=" onClick={onclick} />
             </div>
           </div>
         </div>
